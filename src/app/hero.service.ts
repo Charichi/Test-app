98e7310d17b9168137a1eaf9c32ec38d36ca6c3e
acharichi <acharichi@norsys.fr>
2022-08-24 12:56:45 +0100

    Completing message component and generating routing files

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { HEROES } from "./mock-heroes";
 import { Hero } from "./hero";
 import { Observable, of } from "rxjs";
+import { MessagesService } from "./messages.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('Heroes Service: fetched heroes.')
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
-import {HEROES} from "../mock-heroes";
 import {HeroService} from "../hero.service";
+import {MessagesService} from "../messages.service";
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +13,13 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessagesService) { }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   }
-
-  constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
